cabc4b753c62eeac37608f2d296d446efcbd86b0
hectorcoronado <coronado.hector@gmail.com>
2016-03-27 23:54:04 -0700

    Figured out animation of avatars, need to set limit to div width

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ console.log ("OK");
 
 $(document).on('ready', function() {
 
-  $(document).keyup(function(objEvent) {
+  $(window).keyup (function (objEvent) {
+    console.log ("Ok, we're listening for A");
     if (event.which === 65) {
-      console.log ("Ok, we're listening for A");
+      $('.redPlayer').animate ( { 'left' : '+=75'}, 'linear');
     }
   });
 
-  $(document).keyup(function (objEvent) {
-    if (event.which === 76) {
+
+  $(window).keyup(function (objEvent) {
     console.log ("Ok, we're listening for L");
+    if (event.which === 76) {
+      $('.bluePlayer').animate ( { 'left' : '+=75'}, 'linear');
     }
   });
-
 
 });
+
+
 
 
 //////////////////////////////////
