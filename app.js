7c0c31fe70f97f171486708685cbc232038c64ea
hectorcoronado <coronado.hector@gmail.com>
2016-03-28 00:26:04 -0700

    Getting close to establishing how far red or blue need to go to win

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ console.log ("OK");
 
 $(document).on('ready', function() {
 
-  $(window).keyup (function (objEvent) {
-    console.log ("Ok, we're listening for A");
+  $(window).keyup (function () {
     if (event.which === 65) {
+      console.log ("Ok, we're listening for A");
+
+      //To determine how far red needs to go
+      var redTrackWidth = ( $('#redTrack').width() );
+      console.log ("Red track width is: " + redTrackWidth);
+
+      //To determine red position's value
+      var redPlayerMargin = ( $('.redPlayer').css('left'));
+      console.log ("Red is currently at: " + redPlayerMargin);
+
       $('.redPlayer').animate ( { 'left' : '+=75'}, 'linear');
+
     }
   });
 
 
-  $(window).keyup(function (objEvent) {
-    console.log ("Ok, we're listening for L");
+  $(window).keyup(function () {
     if (event.which === 76) {
+      console.log ("Ok, we're listening for L");
+      var blueTrackWidth = ( $('#blueTrack').width() ); //How far blue needs to go
+      console.log ("Blue track width: " + blueTrackWidth);
       $('.bluePlayer').animate ( { 'left' : '+=75'}, 'linear');
     }
   });
