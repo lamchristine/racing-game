be41a9889a47d96d336df9141e49100a69d40ba5
hectorcoronado <coronado.hector@gmail.com>
2016-03-27 17:59:50 -0700

    Split tracks for player 1 & 2 using Bootstrap

--- app.js
+++ app.js
@@ -2,4 +2,23 @@
 console.log ("OK");
 
 $(document).on('ready', function() {
+
+  $('.red').on('click', function() {
+    $(this).animate (
+      { left: 20 },
+      'slow');
+  });
+
 });
+
+/*
+})
+  $('.red').click (function animateImage() {
+    console.log("Called");
+    $('.red').css({right:"10px"});
+    $('.red').animate({right: '-5%'}, 400, 'swing', function(){animateImage();});
+  });
+
+
+});
+*/
